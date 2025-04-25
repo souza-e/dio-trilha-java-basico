7a2d91d1acb776657a667b91082ecbf6fef074b9
Eduardo <souza-e@hotmail.com>
2025-04-25 16:45:23 -0300

    aula de operadores concluido

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,13 @@
+public class Operadores {
+    
+public static void main(String[] args) {
+    
+int a = 2 , b =  5;
+
+ String resultado = a > b ? "verdadeiro" : "falso"; // operador ternario
+
+System.out.println(resultado); // 5
+
+}
+
+}
