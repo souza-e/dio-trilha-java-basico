a0218e965809d46b604f3822b452ddcb367ad1ba
Eduardo <souza-e@hotmail.com>
2025-04-25 14:40:47 -0300

    aqui acabou a aula de variavel.

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        short numeroCurto = 1;
+        int numeroNormal = 2;
+        long numeroGrande = 3L;
+        float numeroDecimal = 4.5f;
+        String nome = "Lucas";
+        char letra = 'L';
+
+       final double valordepi =3.14;
+
+       System.out.println(valordepi);
     }
 }
